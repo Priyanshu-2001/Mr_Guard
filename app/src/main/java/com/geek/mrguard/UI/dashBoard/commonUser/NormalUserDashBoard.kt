ae5f2acf87cf332841461e0268966ecb092662c5
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-11-12 23:54:34 +0530

    added perfect data while raising alert from normal victim

--- app/src/main/java/com/geek/mrguard/UI/dashBoard/commonUser/NormalUserDashBoard.kt
+++ app/src/main/java/com/geek/mrguard/UI/dashBoard/commonUser/NormalUserDashBoard.kt
@@ -3,6 +3,7 @@ package com.geek.mrguard.UI.dashBoard.commonUser
 import android.app.ProgressDialog
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +14,14 @@ import androidx.databinding.DataBindingUtil
 import com.geek.mrguard.R
 import com.geek.mrguard.SocketIOClient
 import com.geek.mrguard.UI.MainActivity
+import com.geek.mrguard.UI.dashBoard.Police.PoliceDashBoard.Companion.checkPermissionsAvailablity
 import com.geek.mrguard.databinding.ActivityNormalUserDashBoardBinding
+import com.geek.mrguard.services.getUserLocation
+import com.geek.mrguard.utils.PermissionCheck
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.database.FirebaseDatabase
 import io.socket.client.Socket
 import io.socket.emitter.Emitter
@@ -28,6 +36,11 @@ class NormalUserDashBoard : AppCompatActivity() {
     lateinit var pref: SharedPreferences
     lateinit var progressDialog: ProgressDialog
     private lateinit var roomId: String
+    lateinit var locationRequest: LocationRequest
+    lateinit var getuserLocation: getUserLocation
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private var currentLatLng: LatLng? = null
+
     private fun initializeSocket() {
         try {
 //            mSocket = IO.socket("https://police-backend-deploy.herokuapp.com/")
@@ -46,19 +59,19 @@ class NormalUserDashBoard : AppCompatActivity() {
         binding.navLayout.getHeaderView(0).pName.text = currentphoneNumber
         initializeSocket()
         mSocket?.connect()
+        initializeLocationVariables()
+        getCoordinates()
         binding.raiseAlert.setOnStateChangeListener {
             if (it) {
-//                initializeSocket()
-
                 mSocket?.run {
                     // generates random roomID
                     roomId = FirebaseDatabase.getInstance().reference.push().key.toString()
-                    Log.e(TAG, "onCreate: roomID send $roomId")
                     val obj = JSONObject()
                     obj.put("roomId", roomId)
-                    obj.put("lat", "30.722") //TODO need real data
-                    obj.put("long", "76.85")
-                    obj.put("phone_number", "8872365433")
+                    obj.put("lat", currentLatLng?.latitude.toString())
+                    obj.put("long", currentLatLng?.longitude.toString())
+                    obj.put("phone_number", currentphoneNumber)
+                    Log.e(TAG, "onCreate: roomID send $obj")
                     emit("victimJoin", obj)
                 }
                 if (mSocket!!.connected()) {
@@ -103,13 +116,49 @@ class NormalUserDashBoard : AppCompatActivity() {
             binding.drawerLayout.openDrawer(GravityCompat.START)
         }
     }
+
+    private fun getCoordinates() {
+        if(checkPermissionsAvailablity(this)){
+            getCurrentLocationCoordinates()
+        }else{
+            PermissionCheck().requestPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            getCurrentLocationCoordinates()
+        }
+    }
+    fun getCurrentLocationCoordinates(){
+        getuserLocation.checkSettingsAndStartLocationUpdates(
+            locationRequest,
+            fusedLocationProviderClient!!
+        )
+
+        getuserLocation.userLoc.observe(this, {loc->
+            currentLatLng = LatLng(loc.latitude, loc.longitude)
+        })
+    }
+
     private fun openDashboard(){
         startActivity(Intent(this,AnalyticsDashboard::class.java))
     }
     companion object {
         const val TAG = "Normal User DashBoard"
     }
-
+    private fun initializeLocationVariables() {
+        getuserLocation = getUserLocation(this)
+        locationRequest = LocationRequest.create()
+        locationRequest.numUpdates = 1
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
     private val onPoliceJoined =
         Emitter.Listener { args ->
             this.runOnUiThread(Runnable {
